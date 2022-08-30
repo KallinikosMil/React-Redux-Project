f8eab04d1a54ce888930126a52fce23c93f56b86
Drumber <kallinikosmil@gmail.com>
2022-08-30 12:08:54 +0300

    form jsx done? also maybe class component are better!

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+//Field is supposed to be a react component and thats why it has a capital F
+//a component that we are going to render on the screen
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+//reduxForm is a function. it has the same functionality as the connect
+// function that we use with React Redux! (lecture 327)
+
+const StreamCreate = (props) => {
+  const renderInput = ({ input, label }) => {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  };
+  return (
+    <form className="ui form">
+      <Field name="title" component={renderInput} label="Enter Title" />
+      <Field
+        name="description"
+        component={renderInput}
+        label="Enter Description"
+      />
+    </form>
+  );
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'streamCreate',
+})(StreamCreate);
+
+// import React from 'react';
+// import { Field, reduxForm } from 'redux-form';
+// class StreamCreate extends React.Component {
+//   renderInput(formProps) {
+//     return <input {...formProps.input} />;
+//   }
+
+//   render() {
+//     return (
+//       <form>
+//         <Field name="title" component={this.renderInput} />
+//         <Field name="description" component={this.renderInput} />
+//       </form>
+//     );
+//   }
+// }
+// export default reduxForm({
+//   form: 'sreamCreate',
+// })(StreamCreate);
