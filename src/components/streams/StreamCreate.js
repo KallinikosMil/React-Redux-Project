176acedb3cf91fafdce86d4a51d6aad8cebeb26f
Drumber <95851621+DrumberLar@users.noreply.github.com>
2022-08-31 15:17:18 +0300

    Correct way for functional components

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -6,20 +6,29 @@ import { Field, reduxForm } from 'redux-form';
 //reduxForm is a function. it has the same functionality as the connect
 // function that we use with React Redux! (lecture 327)
 
+//When writing functional components with Redux Form you must move
+//the renderInput and onSubmit methods outside of the component.
+// *****************************************************
+// You must define the stateless function outside of your render()
+// method, or else it will be recreated on every render and will force the Field to
+// rerender because its component prop will be different.
+// If you are defining your stateless function inside of render()
+// it will not only be slower, but your input will lose focus
+// whenever the entire form component rerenders.
+//******************************************************
+const renderInput = ({ input, label }) => {
+  return (
+    <div className="field">
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  );
+};
+
+const onSubmit = (formValues) => {
+  console.log(formValues);
+};
 const StreamCreate = (props) => {
-  const renderInput = ({ input, label }) => {
-    return (
-      <div className="field">
-        <label>{label}</label>
-        <input {...input} />
-      </div>
-    );
-  };
-
-  const onSubmit = (formValues) => {
-    console.log(formValues);
-  };
-
   return (
     <form onSubmit={props.handleSubmit(onSubmit)} className="ui form">
       <Field name="title" component={renderInput} label="Enter Title" />
