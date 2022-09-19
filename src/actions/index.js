8d261eba392fc8e29b6fbdd580ece482d9b0a60c
Drumber <kallinikosmil@gmail.com>
2022-09-19 15:08:14 +0300

    some more things done!

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import { get } from 'lodash';
 import {
   CREATE_STREAM,
   DELETE_STREAM,
@@ -21,8 +22,9 @@ export const signOut = () => {
   };
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post('/streams', formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
